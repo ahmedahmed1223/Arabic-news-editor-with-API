9aa5de946a6625ff59a303fe72d77aad19b4239e
Ahmed ahmed <ahmedahmed1223@gmail.com>
2025-07-26 16:43:14 +0000

    Make the website suitable to use with GitHub Page service and check the

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  output: 'export',
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    unoptimized: true,
   },
 };
 
